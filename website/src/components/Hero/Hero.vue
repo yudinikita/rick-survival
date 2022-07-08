d4713cac097c6ff99cbd89dfe64f23df30c4d302
Nikita Yudin <nikitayudin782@gmail.com>
2022-07-08 20:43:46 +0300

    add animation for logo

--- website/src/components/Hero/Hero.vue
+++ website/src/components/Hero/Hero.vue
@@ -6,7 +6,13 @@
     <div class="absolute hero-overlay bg-opacity-20 z-20"></div>
     <div class="absolute hero-content text-center text-neutral-content z-20">
       <div class="max-w-screen-sm">
-        <img class="mx-auto w-3/5" src="~/assets/logo_text.png" alt="Логотип" />
+        <a href="/" class="logo-link">
+          <img
+            class="mx-auto w-3/5"
+            src="~/assets/logo_text.png"
+            alt="Логотип"
+          />
+        </a>
         <HeroSubtitle>Экшен-рогалик • Бесплатная игра</HeroSubtitle>
         <HeroTitle>Выживай и уничтожай тысячи монстров</HeroTitle>
         <HeroDesc>
@@ -38,3 +44,43 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.logo-link {
+  position: relative;
+}
+
+.logo-link > img {
+  transition: cubic-bezier(0.4, 0, 1, 1) 150ms;
+}
+
+.logo-link:hover > img {
+  animation-duration: 1.3s;
+  animation-iteration-count: 1;
+  transform-origin: bottom;
+  animation-name: bounce-4;
+  animation-timing-function: ease;
+}
+
+@keyframes bounce-4 {
+  0% {
+    transform: scale(1, 1) translateY(0);
+  }
+
+  10% {
+    transform: scale(1.1, 0.9) translateY(0);
+  }
+
+  30% {
+    transform: scale(0.9, 1.1) translateY(0);
+  }
+
+  50% {
+    transform: scale(1, 1) translateY(0);
+  }
+
+  100% {
+    transform: scale(1, 1) translateY(0);
+  }
+}
+</style>
